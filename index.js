9a552c50771353dd4cfd2d7733fdc8fe2d4c9187
ttoru96 <ttoru96jk@gmail.com>
2019-09-16 21:20:28 +0900

    Implement the very first server

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+//const express = require("express");
+import express from "express";
+const app = express();
+
+const PORT = 4000;
+
+/*
+function handleListening() {
+  console.log(`Listening on: http://localhost:${PORT}`);
+}
+*/
+const handleListening = () =>
+  console.log(`Listening on: http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send("Hello from home");
+
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
